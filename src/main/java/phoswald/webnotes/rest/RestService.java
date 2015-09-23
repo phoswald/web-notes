1fe27a2dc4b6081b996cea46e6bf104b4aaf58b2
Philip Oswald <philip.oswald@gmail.com>
2015-09-24 01:15:14 +0200

    Added support for LocalDateTime in JPA and JSON

--- src/main/java/phoswald/webnotes/rest/RestService.java
+++ src/main/java/phoswald/webnotes/rest/RestService.java
@@ -1,6 +1,5 @@
 package phoswald.webnotes.rest;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -11,8 +10,11 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.joda.time.LocalDateTime;
 
 import phoswald.webnotes.GreetingService;
 import phoswald.webnotes.entities.Greeting;
@@ -20,52 +22,59 @@ import phoswald.webnotes.entities.Greeting;
 @Path("/service")
 public class RestService {
 
-    private final GreetingService service = new GreetingService();
+    /*
+     * Note: The JSON serializer/deserializer will access the request/response POJOs (such as Greeting)
+     *       using their getters and setters (and not their fields).
+     */
 
-    @GET
-    @Path("/add")
-    @Produces("text/plain")
-    public String add(@QueryParam("name") String name) {
-        Greeting greeting = new Greeting();
-        greeting.setName(name);
-        greeting.setText("Added at " + new Date());
-        service.put(greeting);
-        return "Added greeting for " + name;
-    }
+    private final GreetingService service = new GreetingService();
 
     @GET
     @Path("/greetings")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Greeting> list() {
-        return service.list();
+    public Response list() {
+        List<Greeting> body = service.list();
+        return Response.ok(body).cacheControl(getNoCache()).build();
     }
 
     @POST
     @Path("/greetings")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void post(Greeting greeting) {
+    public Response post(Greeting greeting) {
         greeting.setId(null);
+        greeting.setDate(LocalDateTime.now());
         service.put(greeting);
+        return Response.ok().build();
     }
 
     @GET
     @Path("/greeting/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Greeting get(@PathParam("id") Long id) {
-        return service.get(id);
+    public Response get(@PathParam("id") Long id) {
+        Greeting body = service.get(id);
+        return Response.ok(body).cacheControl(getNoCache()).build();
     }
 
     @PUT
     @Path("/greeting/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void put(@PathParam("id") Long id, Greeting greeting) {
+    public Response put(@PathParam("id") Long id, Greeting greeting) {
         greeting.setId(id);
+        greeting.setDate(LocalDateTime.now());
         service.put(greeting);
+        return Response.ok().build();
     }
 
     @DELETE
     @Path("/greeting/{id}")
-    public void delete(@PathParam("id") Long id) {
+    public Response delete(@PathParam("id") Long id) {
         service.delete(id);
+        return Response.ok().build();
+    }
+
+    private static CacheControl getNoCache() {
+        CacheControl cc = new CacheControl();
+        cc.setNoCache(true);
+        return cc;
     }
 }
